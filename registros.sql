d5e162bd02a7790d491117b864d0309587622344
BarceloCh <barceloch@gmail.com>
2021-02-10 03:24:45 -0500

    Delete registros.sql

--- registros.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS registros (
-  id INTEGER PRIMARY KEY,                                      
-  record_date         DATE NOT NULL,
-  record_time         TEXT NOT NULL,
-  lectura             INTEGER NOT NULL
-)
